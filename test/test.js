6fd2e112820ffff93b391f919fe9582816541021
Dan Putman <dan@codacy.com>
2021-01-08 10:43:04 -0500

    Adding more tests

--- test/test.js
+++ test/test.js
@@ -7,5 +7,9 @@ describe('example', function () {
     it('keeps singular when count is 1', function () {
       assert.strictEqual(pluralize(1, 'cat'), '1 cat');
     });
+
+    it('goes plural when count is 2', function () {
+      assert.strictEqual(pluralize(2, 'cat', 'cats'), '2 cats');
+    });
   });
 });
