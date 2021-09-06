a4d894ff2b4f8d9f097e578edf93fbb8139313c0
Dan Putman <57899154+dan-codacy@users.noreply.github.com>
2021-09-06 09:08:05 -0400

    Update api.js

--- src/client/api.js
+++ src/client/api.js
@@ -1,8 +1,12 @@
-function foo1(a, b, a) {
+function foo1(a, b, c) {
     typeof a === undefined
     console.log("value of the second a:", a);
 }
 
-var bar = function (a, b, a) {
+var bar = function (a, b, c) {
     console.log("value of the second a:", a);
+};
+
+var car = function (a, b, c) {
+    console.log("value of the second c:", c);
 };
