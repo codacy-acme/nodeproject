1cdfb46154ba75d606cf8e2d90b4d8594cd887b2
Hélio Rocha <hjrocha@gmail.com>
2022-01-20 16:33:58 +0000

    increase complexity

--- src/server/lib/people.js
+++ src/server/lib/people.js
@@ -73,41 +73,6 @@ exports.doSomething = function () {
                                                                                                                                     try {
                                                                                                                                         try {
                                                                                                                                             try {
-                                                                                                                                                try {
-                                                                                                                                                    try {
-                                                                                                                                                        try {
-                                                                                                                                                            try {
-                                                                                                                                                                try {
-                                                                                                                                                                    return 1; // 1 is returned but suspended until finally block ends
-                                                                                                                                                                } catch (err) {
-                                                                                                                                                                    return 2;
-                                                                                                                                                                } finally {
-                                                                                                                                                                    return 3; // 3 is returned before 1, which we did not expect
-                                                                                                                                                                }
-                                                                                                                                                                return 1; // 1 is returned but suspended until finally block ends
-                                                                                                                                                            } catch (err) {
-                                                                                                                                                                return 2;
-                                                                                                                                                            } finally {
-                                                                                                                                                                return 3; // 3 is returned before 1, which we did not expect
-                                                                                                                                                            }
-                                                                                                                                                            return 1; // 1 is returned but suspended until finally block ends
-                                                                                                                                                        } catch (err) {
-                                                                                                                                                            return 2;
-                                                                                                                                                        } finally {
-                                                                                                                                                            return 3; // 3 is returned before 1, which we did not expect
-                                                                                                                                                        }
-                                                                                                                                                        return 1; // 1 is returned but suspended until finally block ends
-                                                                                                                                                    } catch (err) {
-                                                                                                                                                        return 2;
-                                                                                                                                                    } finally {
-                                                                                                                                                        return 3; // 3 is returned before 1, which we did not expect
-                                                                                                                                                    }
-                                                                                                                                                    return 1; // 1 is returned but suspended until finally block ends
-                                                                                                                                                } catch (err) {
-                                                                                                                                                    return 2;
-                                                                                                                                                } finally {
-                                                                                                                                                    return 3; // 3 is returned before 1, which we did not expect
-                                                                                                                                                }
                                                                                                                                                 return 1; // 1 is returned but suspended until finally block ends
                                                                                                                                             } catch (err) {
                                                                                                                                                 return 2;
