d2065620f53a9b9651a246551a51e586f3288526
heliocodacy <46491761+heliocodacy@users.noreply.github.com>
2022-09-20 16:29:37 +0100

    Test-FS

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 
+import { promises as fsp } from 'fs';
+import fs from 'fs';
+import path from 'path';
+
 const app = express()
 
 app.use(bodyParser.urlencoded({
     extended: true
  }));
  app.set('json spaces', 2)
+
+
+
+const index = await fsp.readFile(path.resolve(__dirname, './index.html'), 'utf-8');
+const key = fs.readFileSync(path.join(__dirname, './ssl.key'));
+await fsp.writeFile(path.resolve(__dirname, './sitemap.xml'), sitemap);
 
  app.use(bodyParser.json());;
